3ce437eff5b12bb2e1188fed22c28d35ea11b0cf
amitpadaya <amitkpadaya@gmail.com>
2024-07-20 19:06:45 +0530

    my first commit

---
+++ src/main/java/com/my/scope/scopetesting/ScopetestingApplication.java
@@ -0,0 +1,13 @@
+package com.my.scope.scopetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopetestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopetestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/scope/scopetesting/ScopetestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.scope.scopetesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScopetestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
